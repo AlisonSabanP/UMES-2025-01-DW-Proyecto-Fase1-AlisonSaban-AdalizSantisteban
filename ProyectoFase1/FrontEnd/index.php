bf2c49d948baa5907e36a0b43994123cf52e3f24
AdalizSan <adalizsantisteban@gmail.com>
2025-05-09 02:42:56 -0600

    cambios boostrap

--- ProyectoFase1/FrontEnd/index.php
+++ ProyectoFase1/FrontEnd/index.php
@@ -1,35 +1,33 @@
-
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <title>Hotel El Paraiso</title>
+    <title>Hotel El Paraíso</title>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="icon" href="../imagenes/logo_SakuraDreams.ico">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+    <link rel="icon" href="../Imagenes/logo.ico">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
+    <link href="https://fonts.googleapis.com/css2?family=Pacifico&family=Quicksand:wght@400;600&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="../styles.css">
-    <link href="https://fonts.googleapis.com/css2?family=Noto+Sans+JP:wght@400;700&family=Sawarabi+Mincho&display=swap" rel="stylesheet">
+
 </head>
-<body style="background-color:rgb(230, 209, 108);">
-    <nav class="navbar navbar-expand-md navbar-dark fixed-top bg-dark mi-fondo">
+<body>
+    <nav class="navbar navbar-expand-md navbar-dark fixed-top">
         <div class="container-fluid">
-            <div class="container-fluid">
-                <a class="navbar-brand" href="#">
-                    <img src="../imagenes" alt="" width="40" class="d-inline-block align-text-top">
-                    Hotel El Paraiso
-                </a>
-                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarCollapse">
-                    <span class="navbar-toggler-icon"></span>
-                </button>
-            </div>
+            <a class="navbar-brand" href="#">
+                <img src="../Imagenes/logo.ico" alt="Logo" width="40" class="d-inline-block align-text-top">
+                Hotel El Paraíso
+            </a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarCollapse">
+                <span class="navbar-toggler-icon"></span>
+            </button>
             <div class="collapse navbar-collapse" id="navbarCollapse">
-                <ul class="navbar-nav me-auto mb-2 mb-md-0">
+                <ul class="navbar-nav ms-auto mb-2 mb-md-0">
                     <?php 
                         $elementosHeader = [
-                            ["directorio" => "../index.php", "Nombre" => "Inicio", "clase"=> "nav-link"],
-                            ["directorio" =>"#", "Nombre" => "Reserva", "clase"=> "nav-link active"]
+                            ["directorio" => "../index.php", "Nombre" => "Inicio", "clase"=> "nav-link active"],
+                            ["directorio" =>"#", "Nombre" => "Reserva", "clase"=> "nav-link"]
                         ];
                         foreach($elementosHeader as $elemento){
                             echo '<li class="nav-item">';
@@ -41,37 +39,34 @@
             </div>
         </div>
     </nav>
-
-    <div class="container mt-5">
-        <div class="card shadow">
-            <div class="card-header text-center">
-                <h2>Reserva de Hotel</h2>
-            </div>
-            <div class="card-body">
-                <form method="POST" action="">
-                    <div class="mb-3">
-                        <label class="form-label">Nombre completo</label>
-                        <input type="text" name="nombre" class="form-control" required>
-                    </div>
-                    <div class="mb-3">
-                        <label class="form-label">NIT</label>
-                        <input type="text" name="nit" class="form-control" required>
-                    </div>
-                    <div class="mb-3">
-                        <label class="form-label">Fecha de nacimiento</label>
-                        <input type="date" name="fecha_nacimiento" class="form-control" required>
-                    </div>
-                    <div class="mb-3">
-                        <label class="form-label">Fecha de ingreso</label>
-                        <input type="date" name="fecha_ingreso" class="form-control" required>
-                    </div>
-                    <div class="mb-3">
-                        <label class="form-label">Fecha de salida</label>
-                        <input type="date" name="fecha_salida" class="form-control" required>
-                    </div>
-                    <button type="submit" class="btn btn-primary w-100">Reservar</button>
-                </form>
-            </div>
+    <div class="container">
+        <h1 class="tropical-header">Reserva </h1>
+    </div>
+    <div class="card-body">
+        <form method="POST" action="">
+        <div class="mb-3">
+            <label class="form-label">Nombre completo</label>
+            <input type="text" name="nombre" class="form-control" required>
+        </div>
+        <div class="mb-3">
+            <label class="form-label">NIT</label>
+            <input type="text" name="nit" class="form-control" required>
+        </div>
+        <div class="mb-3">
+            <label class="form-label">Fecha de nacimiento</label>
+            <input type="date" name="fecha_nacimiento" class="form-control" required>
+        </div>
+        <div class="mb-3">
+            <label class="form-label">Fecha de ingreso</label>
+            <input type="date" name="fecha_ingreso" class="form-control" required>
+        </div>
+        <div class="mb-3">
+            <label class="form-label">Fecha de salida</label>
+            <input type="date" name="fecha_salida" class="form-control" required>
+        </div>
+        <button type="submit" class="btn btn-primary w-100">Reservar</button>
+        </form>
+        </div>
         </div>
     </div>
 </body>
--- ProyectoFase1/index.php
+++ ProyectoFase1/index.php
@@ -1,34 +1,33 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <title>Hotel El Paraiso</title>
+    <title>Hotel El Paraíso</title>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="icon" href="../imagenes/logo_SakuraDreams.ico">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
-    <link rel="stylesheet" href="../styles.css">
-    <link href="https://fonts.googleapis.com/css2?family=Noto+Sans+JP:wght@400;700&family=Sawarabi+Mincho&display=swap" rel="stylesheet">
+    <link rel="icon" href="Imagenes/logo.ico">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
+    <link href="https://fonts.googleapis.com/css2?family=Pacifico&family=Quicksand:wght@400;600&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="styles.css">
+
 </head>
-<body style="background-color:rgb(230, 209, 108);">
-    <nav class="navbar navbar-expand-md navbar-dark fixed-top bg-dark mi-fondo">
+<body>
+    <nav class="navbar navbar-expand-md navbar-dark fixed-top">
         <div class="container-fluid">
-            <div class="container-fluid">
-                <a class="navbar-brand" href="#">
-                    <img src="../imagenes" alt="" width="40" class="d-inline-block align-text-top">
-                    Hotel El Paraiso
-                </a>
-                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarCollapse">
-                    <span class="navbar-toggler-icon"></span>
-                </button>
-            </div>
+            <a class="navbar-brand" href="#">
+                <img src="Imagenes/logo.ico" alt="Logo" width="40" class="d-inline-block align-text-top">
+                Hotel El Paraíso
+            </a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarCollapse">
+                <span class="navbar-toggler-icon"></span>
+            </button>
             <div class="collapse navbar-collapse" id="navbarCollapse">
-                <ul class="navbar-nav me-auto mb-2 mb-md-0">
+                <ul class="navbar-nav ms-auto mb-2 mb-md-0">
                     <?php 
                         $elementosHeader = [
                             ["directorio" => "#", "Nombre" => "Inicio", "clase"=> "nav-link active"],
-                            ["directorio" =>"FrontEnd/index.php", "Nombre" => "Reserva", "clase"=> "nav-link "]
+                            ["directorio" =>"FrontEnd/index.php", "Nombre" => "Reserva", "clase"=> "nav-link"]
                         ];
                         foreach($elementosHeader as $elemento){
                             echo '<li class="nav-item">';
@@ -40,6 +39,10 @@
             </div>
         </div>
     </nav>
+    <div class="container">
+        <h1 class="tropical-header">Bienvenido al Paraíso 🌴</h1>
+    </div>
+
     <h1>Hola</h1>
 </body>
 </html>
